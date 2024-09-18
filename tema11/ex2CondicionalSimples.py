24500c83b4dfaf592a1f110b1d75087239e5e9e2
Moisés Barsoti <146322015+moisesBarsoti@users.noreply.github.com>
2024-09-18 18:31:56 -0300

    Delete tema11 directory

--- tema11/ex2CondicionalSimples.py
+++
@@ -1,6 +0,0 @@
-# Recebendo valor
-numero = float(input("Digite um numero: "))
-
-# Condição
-if numero <= 35:
-    print(f"O valor {numero} e menor ou igual a 35")
--- tema11/ex3MinhaPreferencia.py
+++
@@ -1,9 +0,0 @@
-# Recebendo valor
-print("")
-pergunta = input("Corinthians e o maior time do mundo ? Digite (s ou n) ")
-
-# Condicao
-if(pergunta == 's'):
-    print("Parabens voce e um genio")
-else:
-    print("Vai estudar um pouco mais ksksks")    
